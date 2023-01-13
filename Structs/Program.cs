467c7d5a8399b1fb8cc847654b9beb37e444e640
thiago <thiago@DESKTOP-M6V0GM4>
2023-01-13 14:03:26 -0300

    Add project files.

---
+++ Structs/Program.cs
@@ -0,0 +1,19 @@
+﻿using Structs;
+using System;
+
+namespace Couser
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            Point p;
+
+            p.X = 10;
+            p.Y = 20;
+
+            Console.WriteLine(p);
+          
+        }
+    }
+}
